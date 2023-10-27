5a4f8c00e6c3cb62dffb1c921b182b94d43579d0
AnselmoNoriega <anselmonoriega3@gmail.com>
2023-10-26 23:46:52 -0700

    Time Manager

--- Assets/Scripts/Buildings/Building.cs
+++ Assets/Scripts/Buildings/Building.cs
@@ -1,7 +1,6 @@
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
-using UnityEngine.WSA;
 
 public class Building : MonoBehaviour
 {
---
+++ Assets/Scripts/Managers/TimeManager.cs
@@ -0,0 +1,41 @@
+using System;
+using TMPro;
+using UnityEngine;
+
+public class TimeManager : MonoBehaviour
+{
+    [SerializeField]
+    private float dailyTime;
+    [SerializeField]
+    private TextMeshProUGUI textTimer;
+
+    private float timePlaying;
+    private TimeSpan elapsTime;
+
+    private int daysPassed = 0;
+
+    private void Awake()
+    {
+        elapsTime = TimeSpan.FromMinutes(dailyTime);
+        timePlaying = dailyTime;
+        textTimer.text = elapsTime.ToString("mm':'ss'.'ff");
+    }
+
+    private void Update()
+    {
+        timePlaying -= Time.deltaTime / 60;
+        elapsTime = TimeSpan.FromMinutes(timePlaying);
+        textTimer.text = elapsTime.ToString("mm':'ss'.'ff");
+
+        if(timePlaying <= 0.0f)
+        {
+            ResetDay();
+        }
+    }
+
+    private void ResetDay()
+    {
+        ++daysPassed;
+        timePlaying = dailyTime;
+    }
+}
