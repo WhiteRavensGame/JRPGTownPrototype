09d5520456949d4962de98508db062cea02eb6c4
AnselmoNoriega <anselmonoriega3@gmail.com>
2023-11-26 20:18:43 -0800

    PauseButton

--- Assets/Scripts/Enums.cs
+++ Assets/Scripts/Enums.cs
@@ -27,5 +27,6 @@ public enum GameStates
 {
     MainScreen,
     PanelInfo,
-    EndOfWeek
+    EndOfWeek,
+    Paused
 }
--- Assets/Scripts/Managers/UIManager.cs
+++ Assets/Scripts/Managers/UIManager.cs
@@ -3,12 +3,18 @@ using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
+    private PlayerManager playerManager;
     [SerializeField] private TextMeshProUGUI _goldText;
     [SerializeField] private TextMeshProUGUI _fishText;
     [SerializeField] private TextMeshProUGUI _ironText;
     [SerializeField] private TextMeshProUGUI _silkText;
 
     [SerializeField] private TextMeshProUGUI _villagerCount;
+
+    private void Awake()
+    {
+        playerManager = ServiceLocator.Get<PlayerManager>();
+    }
 
     public void UpdateResourceText(int gold, int fish, int iron, int silk)
     {
@@ -21,5 +27,22 @@ public class UIManager : MonoBehaviour
     public void UpdateVillagerCount(int left, int total)
     {
         _villagerCount.text = left.ToString() + "/" + total.ToString();
+    }
+
+    public void PauseGame()
+    {
+        if (playerManager.gameState == GameStates.Paused || playerManager.gameState == GameStates.MainScreen)
+        {
+            Time.timeScale = Time.timeScale == 1 ? 0 : 1;
+
+            if (playerManager.gameState == GameStates.MainScreen)
+            {
+                playerManager.gameState = GameStates.Paused;
+            }
+            else
+            {
+                playerManager.gameState = GameStates.MainScreen;
+            }
+        }
     }
 }
