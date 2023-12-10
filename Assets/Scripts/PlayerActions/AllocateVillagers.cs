332e470fc07a0f78f65d2d66483d53976f6265b6
diegobatiz <diegow.batiz@gmail.com>
2023-12-09 17:10:50 -0800

    updating morale ui

--- Assets/Scripts/PlayerActions/AllocateVillagers.cs
+++ Assets/Scripts/PlayerActions/AllocateVillagers.cs
@@ -94,7 +94,7 @@ public class AllocateVillagers : MonoBehaviour
         else if (building.GetBuildingType() == BuildingType.SilkStore)
         {
             int silkLevel = building.GetLevel();
-            float amount = (0.25f * (silkLevel * silkLevel)) - (0.25f * silkLevel) + 0.5f;
+            float amount = (0.5f * (silkLevel * silkLevel)) - (0.5f * silkLevel) + 1f;
             resourcesText.text = amount.ToString();
         }
     }
