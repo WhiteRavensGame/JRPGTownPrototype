099ac65f0a9528bb5fb8e1498c1dcd98c477003d
KebinDev <134655367+KebinDev@users.noreply.github.com>
2023-12-13 14:35:18 -0800

    Dialogue Portraits

--- Assets/Scripts/Dialogue/MainDialogue.cs
+++ Assets/Scripts/Dialogue/MainDialogue.cs
@@ -12,6 +12,8 @@ public class MainDialogue : MonoBehaviour
 
     [Space, Header("UI")]
     [SerializeField] private TextMeshProUGUI dialogueText;
+    [SerializeField] private TextMeshProUGUI displayNameText;
+    [SerializeField] private Animator portraitAnimator;
     [SerializeField] private List<GameObject> buttons;
 
     [Space, Header("Story")]
@@ -24,6 +26,10 @@ public class MainDialogue : MonoBehaviour
     private int currentWord;
 
     private bool loadingText = false;
+
+    private const string SPEAKER_TAG = "speaker";
+    private const string PORTRAIT_TAG = "portrait";
+
 
     public void Enter(TextAsset jsonAsset)
     {
@@ -72,10 +78,38 @@ public class MainDialogue : MonoBehaviour
         if (_currentStory.canContinue || _currentStory.currentChoices.Count > 0)
         {
             LoadTextAnim();
+            HandleTags(_currentStory.currentTags);
         }
         else if (!_currentStory.canContinue && _currentStory.currentChoices.Count <= 0)
         {
             Exit();
+        }
+    }
+
+    private void HandleTags(List<string> currentTags)
+    {
+        foreach (string tag in currentTags)
+        {
+            string[] splitTag = tag.Split(':');
+            if (splitTag.Length != 2)
+            {
+                Debug.LogError("Tag could not be parsed: " + tag);
+            }
+            string tagKey = splitTag[0].Trim();
+            string tagValue = splitTag[1].Trim();
+
+            switch (tagKey)
+            {
+                case SPEAKER_TAG:
+                    displayNameText.text = tagValue;
+                    break;
+                case PORTRAIT_TAG:
+                    portraitAnimator.Play(tagValue);
+                    break;
+                default:
+                    Debug.LogWarning("Tag came in but is not currently being handled: " + tag);
+                    break;
+            }
         }
     }
 
