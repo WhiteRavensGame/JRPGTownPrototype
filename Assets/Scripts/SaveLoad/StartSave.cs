3c0516ce8edf235165c56a981db14c1c7bff1c52
Galalendon <galalendon@gmail.com>
2023-12-13 15:51:29 -0800

    Sound Design

--- Assets/Scripts/SaveLoad/StartSave.cs
+++ Assets/Scripts/SaveLoad/StartSave.cs
@@ -26,6 +26,7 @@ public class StartSave : MonoBehaviour
 
     public void ContinueButton()
     {
+        ServiceLocator.Get<SoundManager>().Play("Yes");
         SceneManager.LoadScene("Introduction");
     }
 
